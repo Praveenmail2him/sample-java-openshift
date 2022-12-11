39c7216b89c8ef2d04857dc03c175084ac7e460f
Praveen Kumar K S <pravks27@in.ibm.com>
2022-12-11 15:29:50 +0530

    version-conflict-removing-target

--- src/main/java/com/ibm/swaggerui/SampleWebApplication.java
+++ src/main/java/com/ibm/swaggerui/SampleWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwaggerUiApplication {
+public class SampleWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwaggerUiApplication.class, args);
+		SpringApplication.run(SampleWebApplication.class, args);
 	}
 
 }
